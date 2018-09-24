8ce453224897a5559abc292871aa458df509994d
hanus hasl <hanus.hasl@mt.com>
2018-09-24 17:36:53 +0200

    Flags and styles added, header enhanced part 1

--- functions.php
+++ functions.php
@@ -10,9 +10,13 @@ function bellashop_enqueue_styles()
     $parent_style = 'parent-style';
     wp_enqueue_style('parent-style', get_template_directory_uri().'/style.css');
     wp_enqueue_style('child-style', get_stylesheet_directory_uri().'/style.css', array($parent_style), wp_get_theme()->get('Version'));
+    // wp_enqueue_style('unishop-styles', get_stylesheet_directory_uri().'/css/styles.min.css', array($parent_style), wp_get_theme()->get('Version'));
+    // wp_enqueue_style('unishop-vendor-style', get_stylesheet_directory_uri().'/css/vendor.min.css', array($parent_style), wp_get_theme()->get('Version'));
 }
 add_action('wp_enqueue_scripts', 'bellashop_enqueue_styles');
 
 require_once get_stylesheet_directory().'/inc/bellashop_nav_menu_walker.php';
 
 add_action('init', 'replace_storefront_header_container_hook');
+
+// add_action('bellashop_before_header', 'bellashop_topbar');
--- inc/bellashop-template-functions.php
+++ inc/bellashop-template-functions.php
@@ -19,3 +19,31 @@ if (!function_exists('bellashop_header_container_close')) {
         echo '</div>';
     }
 }
+
+if (!function_exists('bellashop_topbar')) {
+    function bellashop_topbar()
+    {
+        ?>
+        <div class="topbar">
+            <div class="topbar-column"><a class="hidden-md-down" href="mailto:support@unishop.com"><i class="icon-mail"></i>&nbsp; support@unishop.com</a><a class="hidden-md-down" href="tel:00331697720"><i class="icon-bell"></i>&nbsp; 00 33 169 7720</a><a class="social-button sb-facebook shape-none sb-dark" href="#" target="_blank"><i class="socicon-facebook"></i></a><a class="social-button sb-twitter shape-none sb-dark" href="#" target="_blank"><i class="socicon-twitter"></i></a><a class="social-button sb-instagram shape-none sb-dark" href="#" target="_blank"><i class="socicon-instagram"></i></a><a class="social-button sb-pinterest shape-none sb-dark" href="#" target="_blank"><i class="socicon-pinterest"></i></a>
+            </div>
+            <div class="topbar-column"><a class="hidden-md-down" href="#"><i class="icon-download"></i>&nbsp; Get mobile app</a>
+                <div class="lang-currency-switcher-wrap">
+                <div class="lang-currency-switcher dropdown-toggle"><span class="language"><img alt="English" src="<?php bloginfo('stylesheet_url'); ?>/../img/flags/GB.png"></span><span class="currency">$ USD</span></div>
+                <div class="dropdown-menu">
+                    <div class="currency-select">
+                    <select class="form-control form-control-rounded form-control-sm">
+                        <option value="usd">$ USD</option>
+                        <option value="usd">CZK</option>
+                        <option value="usd">£ UKP</option>
+                    </select>
+                    </div><a class="dropdown-item" href="#">
+                    <img src="<?php bloginfo('stylesheet_url'); ?>/../img/flags/DE.png" alt="Deutsch">Deutsch</a><a class="dropdown-item" href="#">
+                    <img src="<?php bloginfo('stylesheet_url'); ?>/../img/flags/CZ.png" alt="Czech">Česky</a><a class="dropdown-item" href="#">
+                </div>
+                </div>
+            </div>
+        </div>
+    <?php
+    }
+}
