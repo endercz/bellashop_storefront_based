4f6ff07eab2c22e840ebf7b91a85e5efc41946de
hanus hasl <hanus.hasl@mt.com>
2018-09-24 15:24:56 +0200

    initial commit storefront clean

---
+++ functions.php
@@ -0,0 +1,9 @@
+<?php
+
+function bellashop_enqueue_styles()
+{
+    $parent_style = 'parent-style';
+    wp_enqueue_style('parent-style', get_template_directory_uri().'/style.css');
+    wp_enqueue_style('child-style', get_stylesheet_directory_uri().'/style.css', array($parent_style), wp_get_theme()->get('Version'));
+}
+add_action('wp_enqueue_scripts', 'bellashop_enqueue_styles');
