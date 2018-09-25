6f647e4d465a551248a79b2891681ac61d02b4f3
hanus hasl <hanus.hasl@mt.com>
2018-09-25 17:00:17 +0200

    Fix font to support Maven Pro

--- functions.php
+++ functions.php
@@ -10,7 +10,7 @@ function bellashop_enqueue_styles()
     $parent_style = 'parent-style';
     wp_enqueue_style('parent-style', get_template_directory_uri().'/style.css');
     wp_enqueue_style('child-style', get_stylesheet_directory_uri().'/style.css', array($parent_style), wp_get_theme()->get('Version'));
-    wp_enqueue_style('unishop-styles', get_stylesheet_directory_uri().'/css/styles.min.css', array($parent_style), wp_get_theme()->get('Version'));
+    wp_enqueue_style('unishop-styles', get_stylesheet_directory_uri().'/css/styles.css', array($parent_style), wp_get_theme()->get('Version'));
     wp_enqueue_style('unishop-vendor-style', get_stylesheet_directory_uri().'/css/vendor.min.css', array($parent_style), wp_get_theme()->get('Version'));
 }
 add_action('wp_enqueue_scripts', 'bellashop_enqueue_styles');
